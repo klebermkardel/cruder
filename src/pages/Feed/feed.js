612b2db51a70f562c80903973c3b6827d6efaba6
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-12 19:32:37 -0300

    :sparkles: feat: Cria conteúdo HTML do componente Feed

--- src/pages/Feed/feed.js
+++ src/pages/Feed/feed.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 import HeaderMain from '../../components/HeaderMain/HeaderMain';
 
+import More from '../../images/more.svg';
+
 function Feed() {
     return(
         <div>
             <HeaderMain />
+            <main>
+                <div className="cards">
+                    <div className="card">
+                        <header>
+                            <h2>Curso Consumindo API</h2>
+                            <img src={More} />
+                        </header>
+                        <div className="line"></div>
+                        <p>Nesse curso eu ensino vcs a consumirem uma api, com react.js, uma api feita em node js e mongoDB</p>
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
