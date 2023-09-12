37f11c3153bcd1063ff006be0ef6946ffc428bee
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-12 20:11:01 -0300

    :sparkles: feat: Cria estrutura do formulário

--- src/pages/Post/post.js
+++ src/pages/Post/post.js
@@ -2,11 +2,40 @@ import React from 'react';
 
 import Header from '../../components/Header/Header';
 
+import { useForm } from 'react-hook-form';
+
 function Post() {
+    const { register, handleSubmit, formState: { erros } } = useForm()
+
     return(
         <div>
             <Header />
-            <h1>Page Post</h1>
+
+            <main>
+                <div className="card-post">
+                    <h1>Criar postagem</h1>
+                    <div className="line-post"></div>
+                    <div className="card-body-post">
+                        <form>
+                            <div className="fields">
+                                <label>Título:</label>
+                                <input type="text" />
+                            </div>
+                            <div className="fields">
+                                <label>Descrição:</label>
+                                <input type="text" />
+                            </div>
+                            <div className="fields">
+                                <label>Conteúdo:</label>
+                                <textarea type="text"></textarea>
+                            </div>
+                            <div className="btn-post">
+                                <button type="submit">Enviar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>  
+            </main>
         </div>
     )
 }
