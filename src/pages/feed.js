07a7e70c063fd05443258d6846eaeecc2817e528
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-12 17:47:02 -0300

    :sparkles: feat: Cria botões de navegação entre as páginas

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 function Feed() {
     return(
         <div>
             <h1>Page Feed</h1>
+
+            <Link to="/post">
+            <button>Novo Post</button>
+            </Link>
         </div>
     )
 }
--- src/pages/post.js
+++ src/pages/post.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 function Post() {
     return(
         <div>
             <h1>Page Post</h1>
+
+            <Link to="/">
+                <button>Voltar ao feed</button>
+            </Link>
         </div>
     )
 }
