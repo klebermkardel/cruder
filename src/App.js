639eaa87ca5303e132e34a884c80e037f83154a5
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-12 17:40:05 -0300

    :sparkles: feat: Cria rotas da aplicação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Post from './pages/post'
+import Edit from './pages/edit'
+import LerMais from './pages/lermais'
+import Feed from './pages/feed'
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Feed />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/lermais" element={<LerMais />} />
+      </Routes>
+    </Router>
   );
 }
 
